ac872b042a0793edf9b6b632fdd856cb50b83347
Aashish Bharath <82909645+AashishBharath@users.noreply.github.com>
2025-03-26 20:13:28 -0500

    Added loading component to Habit Cards and fixed context issue. (#97)

--- components/ProfileSettings/HeatMapDisplay.tsx
+++ components/ProfileSettings/HeatMapDisplay.tsx
@@ -10,12 +10,10 @@ import {
 import React, { useState, useEffect } from "react";
 import { Habit } from "@/types/types";
 import { useGlobalContext } from "@/context/Context";
-import { DateTime } from "luxon";
 import { FlashList } from "@shopify/flash-list";
 import HabitHeatMap from "./HabitHeatMap";
 import CalendarButton from "../CalendarButton";
 import HabitYearView from "../HabitYearView";
-import dayjs from "dayjs";
 import TrackingWelcome from "./TrackingWelcome";
 import Entypo from "@expo/vector-icons/Entypo";
 import { useHabitContext } from "@/context/HabitContext";
